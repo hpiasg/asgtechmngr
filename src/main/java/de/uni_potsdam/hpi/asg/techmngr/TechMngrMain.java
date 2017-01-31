749c85124b1e91871ff9e51f7198096cc496efb3
Norman Kluge <norman.kluge@hpi.de>
2017-01-31 16:41:48 +0100

    make final strings final

--- src/main/java/de/uni_potsdam/hpi/asg/techmngr/TechMngrMain.java
+++ src/main/java/de/uni_potsdam/hpi/asg/techmngr/TechMngrMain.java
@@ -31,8 +31,8 @@ import de.uni_potsdam.hpi.asg.common.technology.TechnologyDirectory;
 
 public class TechMngrMain {
 
-    public static String techdir      = "$BASEDIR/tech";
-    public static String balsatechdir = "$BASEDIR/tools/balsa/share/tech";
+    public static final String techdir      = "$BASEDIR/tech";
+    public static final String balsatechdir = "$BASEDIR/tools/balsa/share/tech";
 
     public static void main(String[] args) {
         LoggerHelper.initLogger(3, null, false);
